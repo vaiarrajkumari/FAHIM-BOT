59e475c3165871420c4a91ed2e01fdb4fc6128e7
FAHIM AHMED RAJ <fahimahmedraj207@gmail.com>
2026-06-21 19:22:38 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61584807686126",];
+		const permission = ["100002237017720",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("𝐤𝐡𝐚𝐧𝐤𝐢𝐫 𝐜𝐡𝐞𝐥𝐞 𝐣𝐚 𝐯𝐚𝐠𝐠𝐠 𝐛𝐨𝐭 𝐤𝐢 𝐭𝐨𝐫 𝐛𝐚𝐩𝐞𝐫...🖕😒", event.threadID, event.messageID);
 		}
